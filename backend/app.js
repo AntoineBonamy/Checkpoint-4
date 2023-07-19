0826d22630e44d9910c3e42f1682a763e030ec84
Elindanova <bonamy.antoine@hotmail.fr>
2023-07-19 13:36:21 +0200

    Data fetched and .map is ok. Cards have same size

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,6 @@
+const fs = require("node:fs");
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
@@ -8,13 +11,36 @@ const cors = require("cors");
 
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+        origin: process.env.FRONTEND_URL ?? "http://localhost:5173",
         optionSuccessStatus: 200,
     })
-)
+);
 
 const router = require("./routes/routes");
 
 app.use(router);
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+const reactIndexFile = path.join(
+    __dirname,
+    "..",
+    "..",
+    "frontend",
+    "dist",
+    "index.html"
+  );
+
+  if (fs.existsSync(reactIndexFile)) {
+    // serve REACT resources
+  
+    app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
+  
+    // redirect all requests to the REACT index file
+  
+    app.get("*", (req, res) => {
+      res.sendFile(reactIndexFile);
+    });
+  }
 
 module.exports = app;
